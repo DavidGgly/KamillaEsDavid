1e262383d5f6217ccb4d46e9c1bc5ee10275da59
DavidGgly <82330981+DavidGgly@users.noreply.github.com>
2021-04-12 22:50:35 +0200

    Updated process.php
    Added a basic if statement to avoid the most obvious SQL injection attacks.

--- process.php
+++ process.php
@@ -2,7 +2,11 @@
 <body>
 
 <?php
-    
+    if (isset($_REQUEST['guestname'])) {
+        $guestname = preg_replace('/[^a-zA-Z0-9\ ]/', '', $_REQUEST['guestname']);
+        $guestname = htmlspecialchars($guestname);
+    }
+
 ?>
 
 </body>
